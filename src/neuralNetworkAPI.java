36ab962966cb1d12fb78efb76cc4fbb780b9fe85
Yousuf Ali <yhali@uwaterloo.ca>
2018-08-31 17:27:00 -0400

    Applied changes to API view. Version 1.2

--- src/neuralNetworkAPI.java
+++ src/neuralNetworkAPI.java
@@ -1,6 +1,33 @@
 package neuralnetworkapi.src;
 
+import neuralnetwork.src.imports.network;
+
 public class neuralNetworkAPI{
-    
+
+    network neuralNetwork;
+
+    //Constructor
+    public void neuralNetworkAPI(){
+        neuralNetwork = new network();
+    }
+
+    public void neuralNetworkAPI(network duplicateNeuralNetwork){
+        neuralNetwork = new network(duplicateNeuralNetwork);
+    }
+
+    public void printNetwork(network neuralNetwork){
+        try{
+            neuralNetwork.print();
+        }
+        catch(Exception nullPointer){
+            System.out.println("Error: Please ensure that the network is initialized properly.");
+        }
+    }
+
+    public void assignNumberOfLayers(Integer layersCount){
+
+    }
+
+    public void ()
 }
 
